054fb6948fa1a1c9ea790b2f50b93c66d3310d6f
Hadi Sql <hadi.sqalli@laplateforme.io>
2024-07-04 15:26:09 +0200

    docker container with tw-css

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM alpine:latest
 
 # Install necessary packages
-RUN apk update && apk add --no-cache ffmpeg python3 py3-pip py3-virtualenv
+RUN apk update && apk add --no-cache ffmpeg python3 py3-pip py3-virtualenv curl
 
 # Create a virtual environment
 RUN python3 -m venv /opt/venv
@@ -10,20 +10,29 @@ RUN python3 -m venv /opt/venv
 # Activate the virtual environment and install Flask
 RUN /opt/venv/bin/pip install flask
 
-# Copy the script and .env file into the container
-COPY app.py /usr/local/bin/app.py
-COPY templates /usr/local/bin/templates
-COPY .env /usr/local/bin/.env
+# Set the working directory
+WORKDIR /app
 
+# Copy the script and .env file into the container
+COPY app.py /app/
+COPY templates /app/templates/
+COPY .env /app/.env
+COPY tailwind.config.js /app/
+COPY static /app/static/
+
+# Download and install Tailwind CSS standalone executable
+RUN curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64 \
+    && chmod +x tailwindcss-linux-x64 \
+    && mv tailwindcss-linux-x64 /usr/local/bin/tailwindcss 
+
+# compile tailwind css output
+RUN /usr/local/bin/tailwindcss -i /app/static/input.css -o /app/static/output.css --minify
 # Set the environment variable to use the virtual environment
 ENV VIRTUAL_ENV=/opt/venv
 ENV PATH="/opt/venv/bin:$PATH"
 
 # Make the app.py executable
-RUN chmod +x /usr/local/bin/app.py
-
-# Set the working directory
-WORKDIR /usr/local/bin
+RUN chmod +x /app/app.py
 
 # Expose the web UI port
 EXPOSE 8217
--- app.py
+++ app.py
@@ -2,26 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 import subprocess
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='static')
 recording_process = None
 
 @app.route('/')
 def index():
+    # Compile Tailwind CSS
+    # subprocess.run(['tailwindcss', '-i', 'static/input.css', '-o', 'static/output.css', '--minify'])
     return render_template('index.html')
 
 @app.route('/start', methods=['POST'])
 def start_recording():
     global recording_process
+
+    rtsp_url = os.environ['RTSP_URL']
+    segment_time = os.environ['SEGMENT_TIME']
+    dest_folder = os.environ['DEST_FOLDER']
+    video_format = os.environ['VIDEO_FORMAT']
     if recording_process is None:
-        ffmpeg_cmd = [
-            'ffmpeg', '-rtsp_transport', 'tcp', '-y', '-timeout', '1000000',
-            '-i', os.environ['RTSP_URL'], '-c:v', 'copy', '-an', '-f', 'segment',
-            '-segment_time', os.environ['SEGMENT_TIME'], '-segment_atclocktime', '1',
-            '-strftime', '1', f"{os.environ['DEST_FOLDER']}/%Y-%m-%d_%H-%M-%S.{os.environ['VIDEO_FORMAT']}",
-            '-loglevel', 'info'
-        ]
-        recording_process = subprocess.Popen(ffmpeg_cmd)
+        # Construct the ffmpeg command
+        ffmpeg_cmd = f"ffmpeg -rtsp_transport tcp -y -timeout 1000000 -i '{rtsp_url}' -c:v copy -an -f segment -segment_time {segment_time} -segment_atclocktime 1 -strftime 1 '{dest_folder}/%Y-%m-%d_%H-%M-%S.{video_format}' -loglevel info"
+        # Start the ffmpeg process
+        recording_process = subprocess.Popen(ffmpeg_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
     return redirect(url_for('index'))
 
 @app.route('/stop', methods=['POST'])
@@ -29,8 +32,9 @@ def stop_recording():
     global recording_process
     if recording_process is not None:
         recording_process.terminate()
+        recording_process.wait()
         recording_process = None
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8217)
+    app.run(host='0.0.0.0', port=8217, debug=os.environ['DEBUG'])
